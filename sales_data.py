59592a015ab5eb91b5e1ac87439684f9af0041e8
Virgil Ungureanu <uvirgil1901@gmail.com>
2025-03-04 02:11:06 +0200

    Added initial file sales_data.py

---
+++ sales_data.py
@@ -0,0 +1,11 @@
+## Data sales for products, months ang quantity
+sales_data = [
+    {'product': 'Smartphone', 'month': 'January', 'quantity': 150},
+    {'product': 'Laptop', 'month': 'January', 'quantity': 80},
+    {'product': 'Tablet', 'month': 'January', 'quantity': 50},
+    {'product': 'Smartphone', 'month': 'February', 'quantity': 200},
+    {'product': 'Laptop', 'month': 'February', 'quantity': 90},
+    {'product': 'Tablet', 'month': 'February', 'quantity': 60},
+    {'product': 'Smartphone', 'month': 'March', 'quantity': 250},
+    {'product': 'Laptop', 'month': 'March', 'quantity': 100},
+    {'product': 'Tablet', 'month': 'March', 'quantity': 70},] 
