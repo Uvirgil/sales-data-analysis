59b0bf0fb579953fc2047c14bba6c1578f40f09a
Virgil Ungureanu <uvirgil1901@gmail.com>
2025-03-04 02:27:05 +0200

    Added function total_sales

--- sales_data.py
+++ sales_data.py
@@ -9,3 +9,24 @@ sales_data = [
     {'product': 'Smartphone', 'month': 'March', 'quantity': 250},
     {'product': 'Laptop', 'month': 'March', 'quantity': 100},
     {'product': 'Tablet', 'month': 'March', 'quantity': 70},] 
+
+product_sales = {}
+month_sales = {}
+
+def total_sales(sales_data):
+
+    for entry in sales_data:
+        product = entry["product"]
+        month = entry["month"]
+        quantity = entry["quantity"]
+        
+        if product in product_sales:
+            product_sales[product] += quantity
+        else:
+            product_sales[product] = quantity
+            
+            
+        if month in month_sales:
+            month_sales[month] += quantity
+        else:
+            month_sales[month] = quantity
